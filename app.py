52286a4de07d3477e94151f6bb0f9484d31bc9c6
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-07-19 14:26:11 +0300

    add new code 2

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        return redirect(url_for('hello', name=name))
+    return render_template('index.html', form=form)
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
